fb301f2427c2da096f32a1b19ad99033ad0afdec
yketu <157855738+yketu@users.noreply.github.com>
2026-04-23 10:34:28 -0600

    Update Movie component to fetch movie data on mount
    Refactor Movie component to use useEffect for fetching movie data.

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import { fetchMovie } from "../actions/movieActions";
-import MovieDetail from "../components/moviedetail"
-
-// support routing
+import MovieDetail from "../components/moviedetail";
 
 function Movie(props) {
-    const [selectedMovie] = useState(props.selectedMovie);
     const params = useParams();
     const movieId = params.movieId;
-    console.log(movieId);
     const dispatch = useDispatch();
-    if (selectedMovie == null) {
-        dispatch(fetchMovie(movieId));
-    }
 
-    return (<MovieDetail movieId={movieId} />)
+    useEffect(() => {
+        dispatch(fetchMovie(movieId));
+    }, [dispatch, movieId]);
+
+    return <MovieDetail movieId={movieId} />;
 }
 
 export default Movie;
