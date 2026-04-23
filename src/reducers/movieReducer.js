fa0d2819ad3abf3e3f64f02e535771f899fb91c0
yketu <157855738+yketu@users.noreply.github.com>
2026-04-23 12:06:38 -0600

    Update movieReducer.js

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -11,7 +11,7 @@ const movieReducer = (state = initialState, action) => {
       switch(action.type) {
             case constants.FETCH_MOVIES:
                   updated['movies'] = action.movies;
-                  updated['selectedMovie'] = action.movies[0];
+                  //updated['selectedMovie'] = action.movies[0];
                   return updated;
             case constants.SET_MOVIE:
                   updated['selectedMovie'] = action.selectedMovie;
